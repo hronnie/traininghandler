b4aa049962f007ff83a72a21a6273418e1485fd8
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2014-02-20 16:13:23 +0000

    fixing db table creations

--- src/main/sql/schema/UserTraining.sql
+++ src/main/sql/schema/UserTraining.sql
@@ -3,10 +3,8 @@ DROP TABLE IF EXISTS UserTraining;
 CREATE TABLE UserTraining (
     userId BIGINT(20) NOT NULL,
     trainingId BIGINT(20) NOT NULL,
-    userId BIGINT(20) NOT NULL,
-    tainingId BIGINT(20) NOT NULL,
     
     PRIMARY KEY (userId, trainingId),
-    FOREIGN KEY FK_UserTraining_User (userId) REFERENCES User(userId),
-    FOREIGN KEY FK_UserTraining_Training (trainingId) REFERENCES Training(tainingId)
+    FOREIGN KEY FK_UserTraining_User (userId) REFERENCES User(userId) ON DELETE CASCADE,
+    FOREIGN KEY FK_UserTraining_Training (trainingId) REFERENCES Training(trainingId) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/main/sql/setup.sql
+++ src/main/sql/setup.sql
@@ -2,5 +2,7 @@ create user 'training'@'%' identified by 'rootpass';
 create user 'training'@'localhost' identified by 'rootpass';
 
 create database traininghandler;
+create database db_bootstrap;
 
 grant all on traininghandler.* to training;
+grant all on db_bootstrap.* to training;
