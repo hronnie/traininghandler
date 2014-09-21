44ecbd6974b7d2fd3e6f2317531fa9a4dd67bd41
Aron Harsfalvi <hronnie@gmail.com>
2014-09-21 11:28:24 +0100

    [262] - add dbunit config (doesn't work yet)

--- src/main/java/com/codeproj/traininghandler/dao/TrainingTypeDAO.java
+++ src/main/java/com/codeproj/traininghandler/dao/TrainingTypeDAO.java
@@ -1,9 +1,14 @@
 package com.codeproj.traininghandler.dao;
 
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.codeproj.traininghandler.dao.common.IOperations;
 import com.codeproj.traininghandler.model.TrainingType;
 
+@Service
+@Transactional
 public interface TrainingTypeDAO extends IOperations<TrainingType> {
 
 
--- src/test/java/com/codeproj/traininghandler/dao/TrainingTypeDAOTest.java
+++ src/test/java/com/codeproj/traininghandler/dao/TrainingTypeDAOTest.java
@@ -1,18 +1,40 @@
 package com.codeproj.traininghandler.dao;
 
-import static org.junit.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.codeproj.traininghandler.dao.impl.TrainingTypeDAOImpl;
 import com.codeproj.traininghandler.model.TrainingType;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
 
-@RunWith(MockitoJUnitRunner.class)
+
+
+// For reference: https://github.com/hronnie/spring-test-dbunit
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration({"/datasource-testcontext.xml"})
+//@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+//		DbUnitTestExecutionListener.class })
 public class TrainingTypeDAOTest {
 
+	@Autowired
 	public TrainingTypeDAO trainingTypeDAO;
 	
 	@Before
@@ -21,13 +43,16 @@ public class TrainingTypeDAOTest {
 	}
 
 	//@Test
+	@DatabaseSetup("TrainingTypeCreateSampleData.xml")
+	@ExpectedDatabase("TrainingTypeCreateExpData.xml")
 	public void testCreateTraingType() {
 		TrainingType trainingType = new TrainingType("Name", "levelNo", "description");
+		trainingTypeDAO.create(trainingType);
 	}
 	
-	@Test
+	//@Test
 	public void test() {
 		
 	}
-
+	
 }
