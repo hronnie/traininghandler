9e97ba8142bb23413cfd9d6b00dbfaae14b19b22
Aron Harsfalvi <hronnie@gmail.com>
2014-10-21 20:41:00 +0100

    [260] - adds training type data for db

---
+++ src/main/sql/data/TrainingTypeData.sql
@@ -0,0 +1,8 @@
+INSERT INTO TrainingType VALUES (1, '1-es tanfolyam', '1', 'Ez egy teszt leiras az 1-es tanfolyamhoz');
+INSERT INTO TrainingType VALUES (2, '2-es tanfolyam', '2', 'Ez egy teszt leiras az 2-es tanfolyamhoz');
+INSERT INTO TrainingType VALUES (3, '3-es tanfolyam', '3', 'Ez egy teszt leiras az 3-as tanfolyamhoz');
+INSERT INTO TrainingType VALUES (4, '4-es tanfolyam', '4', 'Ez egy teszt leiras az 4-es tanfolyamhoz');
+INSERT INTO TrainingType VALUES (5, '5-es tanfolyam', '5', 'Ez egy teszt leiras az 5-es tanfolyamhoz');
+INSERT INTO TrainingType VALUES (6, '6-es tanfolyam', '6', 'Ez egy teszt leiras az 6-os tanfolyamhoz');
+INSERT INTO TrainingType VALUES (7, '7-es tanfolyam', '7', 'Ez egy teszt leiras az 7-es tanfolyamhoz');
+INSERT INTO TrainingType VALUES (8, '8-es tanfolyam', '8', 'Ez egy teszt leiras az 8-es tanfolyamhoz');
