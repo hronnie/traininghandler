b68a0b3303ce489e5e52533db610b7bde59a5839
Aron Harsfalvi <hronnie@gmail.com>
2014-10-24 19:14:16 +0100

    [261] - adds training type controller

---
+++ src/main/java/com/codeproj/traininghandler/controller/TrainingTypeController.java
@@ -0,0 +1,23 @@
+package com.codeproj.traininghandler.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/trainingType")
+public class TrainingTypeController {
+	private static final Logger logger = Logger.getLogger(IndexController.class);
+	
+	@RequestMapping(method = RequestMethod.GET)
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+
+		return new ModelAndView("trainingType");
+	}
+}
