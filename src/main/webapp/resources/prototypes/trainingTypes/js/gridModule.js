9af10734ba4c60da0ee71d1cd3cc3ba132c94262
Aron Harsfalvi <a.harsfalvi@kainos.com>
2014-10-24 17:19:07 +0100

    [260] - cleanup angular events

--- src/main/webapp/resources/prototypes/trainingTypes/js/gridModule.js
+++ src/main/webapp/resources/prototypes/trainingTypes/js/gridModule.js
@@ -4,27 +4,27 @@ app.run(function(editableOptions) {
   editableOptions.theme = 'bs3';
 });
 */
+
 app.controller('thTrainingTypeController', function($scope, $filter, Restangular) {
 	
-	var trainingTypesRest = Restangular.allUrl('t', 'http://localhost:8080/traininghandler/rest/trainingtype/getAll');
-	$scope.trainingTypes = trainingTypesRest.getList();
-	 
-//	 [
-//    {id: 1, name: '1 as/es tanfolyam', levelNo: '1', description: 'teszt leiras'},
-//    {id: 2, name: '2 as/es tanfolyam', levelNo: '2', description: 'teszt leiras'},
-//    {id: 3, name: '3 as/es tanfolyam', levelNo: '3', description: 'teszt leiras'},
-//    {id: 4, name: '4 as/es tanfolyam', levelNo: '4', description: 'teszt leiras'},
-//    {id: 5, name: '5 as/es tanfolyam', levelNo: '4', description: 'teszt leiras'},
-//    {id: 6, name: '6 as/es tanfolyam', levelNo: '5', description: 'teszt leiras'},
-//    {id: 7, name: '7 as/es tanfolyam', levelNo: '6', description: 'teszt leiras'},
-//    {id: 8, name: '8 as/es tanfolyam', levelNo: '7', description: 'teszt leiras'},
-//    {id: 9, name: '9 as/es tanfolyam', levelNo: '8', description: 'teszt leiras'},
-//    {id: 10, name: '10 as/es tanfolyam', levelNo: '9', description: 'teszt leiras'},
-//    {id: 11, name: '11 as/es tanfolyam', levelNo: '10', description: 'teszt leiras'},
-//    {id: 12, name: '12 as/es tanfolyam', levelNo: '11', description: 'teszt leiras'},
-//    {id: 13, name: '13 as/es tanfolyam', levelNo: '12', description: 'teszt leiras'},
-//    {id: 14, name: '14 as/es tanfolyam', levelNo: '13', description: 'teszt leiras'},
-//  ]; 
+	//var trainingTypesRest = Restangular.allUrl('t', 'http://66.172.27.212:8080/traininghandler/rest/trainingtype/getAll');
+	$scope.trainingTypes =
+	 [
+    {id: 1, name: '1 as/es tanfolyam', levelNo: '1', description: 'teszt leiras'},
+    {id: 2, name: '2 as/es tanfolyam', levelNo: '2', description: 'teszt leiras'},
+    {id: 3, name: '3 as/es tanfolyam', levelNo: '3', description: 'teszt leiras'},
+    {id: 4, name: '4 as/es tanfolyam', levelNo: '4', description: 'teszt leiras'},
+    {id: 5, name: '5 as/es tanfolyam', levelNo: '4', description: 'teszt leiras'},
+    {id: 6, name: '6 as/es tanfolyam', levelNo: '5', description: 'teszt leiras'},
+    {id: 7, name: '7 as/es tanfolyam', levelNo: '6', description: 'teszt leiras'},
+    {id: 8, name: '8 as/es tanfolyam', levelNo: '7', description: 'teszt leiras'},
+    {id: 9, name: '9 as/es tanfolyam', levelNo: '8', description: 'teszt leiras'},
+    {id: 10, name: '10 as/es tanfolyam', levelNo: '9', description: 'teszt leiras'},
+    {id: 11, name: '11 as/es tanfolyam', levelNo: '10', description: 'teszt leiras'},
+    {id: 12, name: '12 as/es tanfolyam', levelNo: '11', description: 'teszt leiras'},
+    {id: 13, name: '13 as/es tanfolyam', levelNo: '12', description: 'teszt leiras'},
+    {id: 14, name: '14 as/es tanfolyam', levelNo: '13', description: 'teszt leiras'},
+  ]; 
 
 
   $scope.checkName = function(data) {
