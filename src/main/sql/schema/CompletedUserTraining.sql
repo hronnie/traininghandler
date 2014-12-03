b70cffb1f9e2433984c6bf4f5208e776c0357015
Aron Harsfalvi <a.harsfalvi@kainos.com>
2014-12-03 17:15:43 +0000

    [308] - create new completed user training table

---
+++ src/main/sql/schema/CompletedUserTraining.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS CompletedUserTraining;
+
+CREATE TABLE CompletedUserTraining (
+    completedUserTrainingId BIGINT(20) NOT NULL,
+    trainingTypeId BIGINT(20) NOT NULL,
+    completedDate DATETIME NOT NULL,
+    
+    PRIMARY KEY (completedUserTrainingId),
+    FOREIGN KEY FK_CompletedUserTraining_TrainingType (trainingTypeId) REFERENCES TrainingType(trainingTypeId)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
