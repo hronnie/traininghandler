a8636c94d6aa86cfc60fb8a1f33b1cad0c6b651d
Aron Harsfalvi <a.harsfalvi@kainos.com>
2015-01-16 18:15:40 +0000

    [335] - adds draft of excel sheet read

--- src/main/java/com/codeproj/traininghandler/controller/ImportTrainingController.java
+++ src/main/java/com/codeproj/traininghandler/controller/ImportTrainingController.java
@@ -1,9 +1,17 @@
 package com.codeproj.traininghandler.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,10 +20,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.codeproj.traininghandler.dto.TrainingExcelDto;
+
+
 @Controller
 @RequestMapping("/importTraining")
 public class ImportTrainingController {
 	private static final Logger logger = Logger.getLogger(ImportTrainingController.class);
+	
+	public static final int EXCEL_TRAINING_START_ROW_INDEX = 3;
+	public static final int EXCEL_TRAINING_START_COLUMN_INDEX = 2;
+	public static final int EXCEL_TRAINING_NAME_COL_INDEX = 0;
+	public static final int EXCEL_TRAINING_POST_CODE_COL_INDEX = 1;
+	public static final int EXCEL_TRAINING_ADDRESS_COL_INDEX = 2;
+	public static final int EXCEL_TRAINING_PHONE_NO_COL_INDEX = 3;
+	public static final int EXCEL_TRAINING_EMAIL_COL_INDEX = 4;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
@@ -32,7 +51,41 @@ public class ImportTrainingController {
 			@RequestParam("day") String day, 
 			@RequestParam("importFile") MultipartFile importFile) throws Exception {
 		
+		
+		FileInputStream fileIn = null;
+
+        try
+        {
+        	File file = new File(importFile.getOriginalFilename());
+        	importFile.transferTo(file);
+        	fileIn = new FileInputStream(file);
+        	
+        	Workbook wb = WorkbookFactory.create(fileIn);
+            Sheet sheet = wb.getSheetAt(0);
+            
+            Row row = sheet.getRow(EXCEL_TRAINING_START_ROW_INDEX);
+//            Cell cell = row.getCell(5);
+            TrainingExcelDto training = new TrainingExcelDto(row.getCell(EXCEL_TRAINING_START_COLUMN_INDEX + EXCEL_TRAINING_NAME_COL_INDEX).getStringCellValue(), 
+            		row.getCell(EXCEL_TRAINING_START_COLUMN_INDEX + EXCEL_TRAINING_POST_CODE_COL_INDEX).getStringCellValue(), 
+            		row.getCell(EXCEL_TRAINING_START_COLUMN_INDEX + EXCEL_TRAINING_ADDRESS_COL_INDEX).getStringCellValue(), 
+            		row.getCell(EXCEL_TRAINING_START_COLUMN_INDEX + EXCEL_TRAINING_PHONE_NO_COL_INDEX).getStringCellValue(), 
+            		row.getCell(EXCEL_TRAINING_START_COLUMN_INDEX + EXCEL_TRAINING_EMAIL_COL_INDEX).getStringCellValue());
+        } finally {
+            if (fileIn != null) {
+            	fileIn.close();
+            }
+        }
+		
 		logger.debug("Processing POST request for importTraining page..");
 		 return "You successfully uploaded file";
+	}
+	
+	public static boolean isRowEmpty(Row row) {
+	    for (int c = row.getFirstCellNum(); c < row.getLastCellNum(); c++) {
+	        Cell cell = row.getCell(c);
+	        if (cell != null && cell.getCellType() != Cell.CELL_TYPE_BLANK)
+	            return false;
+	    }
+	    return true;
 	}
 }
---
+++ src/main/java/com/codeproj/traininghandler/dto/TrainingExcelDto.java
@@ -0,0 +1,131 @@
+package com.codeproj.traininghandler.dto;
+
+public class TrainingExcelDto {
+	private String name;
+	private String postCode;
+	private String address;
+	private String phoneNo;
+	private String email;
+	public String getName() {
+		return name;
+	}
+	
+	public TrainingExcelDto() {
+		// empty constructor
+	}
+	
+	public TrainingExcelDto(String name, String postCode, String address,
+			String phoneNo, String email) {
+		this.name = name;
+		this.postCode = postCode;
+		this.address = address;
+		this.phoneNo = phoneNo;
+		this.email = email;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPostCode() {
+		return postCode;
+	}
+	
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phoneNo == null) ? 0 : phoneNo.hashCode());
+		result = prime * result
+				+ ((postCode == null) ? 0 : postCode.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof TrainingExcelDto)) {
+			return false;
+		}
+		TrainingExcelDto other = (TrainingExcelDto) obj;
+		if (address == null) {
+			if (other.address != null) {
+				return false;
+			}
+		} else if (!address.equals(other.address)) {
+			return false;
+		}
+		if (email == null) {
+			if (other.email != null) {
+				return false;
+			}
+		} else if (!email.equals(other.email)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (phoneNo == null) {
+			if (other.phoneNo != null) {
+				return false;
+			}
+		} else if (!phoneNo.equals(other.phoneNo)) {
+			return false;
+		}
+		if (postCode == null) {
+			if (other.postCode != null) {
+				return false;
+			}
+		} else if (!postCode.equals(other.postCode)) {
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "TrainingExcelDto [name=" + name + ", postCode=" + postCode
+				+ ", address=" + address + ", phoneNo=" + phoneNo + ", email="
+				+ email + "]";
+	}
+}
