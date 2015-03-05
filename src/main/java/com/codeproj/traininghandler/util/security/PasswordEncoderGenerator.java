9f1bcfef860fe68f6dfe5fac523946430a861d42
Aron Harsfalvi <a.harsfalvi@kainos.com>
2015-03-05 10:45:29 +0000

    configures spring security to use encrypted passwords

---
+++ src/main/java/com/codeproj/traininghandler/util/security/PasswordEncoderGenerator.java
@@ -0,0 +1,16 @@
+package com.codeproj.traininghandler.util.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderGenerator {
+
+	public static void main(String[] args) {
+
+		String password = "password";
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(password);
+
+		System.out.println(hashedPassword);
+
+	}
+}
